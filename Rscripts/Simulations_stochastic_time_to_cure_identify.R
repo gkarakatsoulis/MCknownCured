f3feb3ccf98ac833bf8c4a22ed177b924743a3d7
Georgios Karakatsoulis <83765859+gkarakatsoulis@users.noreply.github.com>
2024-12-10 12:35:50 +0200

    Simulations_stochastic_time_to_cure_identify.R

--- Rscripts/Simulations_stochastic_time_to_cure_identify.R
+++ Rscripts/Simulations_stochastic_time_to_cure_identify.R
@@ -1,9 +1,9 @@
 library(dplyr)
 
 
-# To simulate the (randomly right) censored observations, we need to first simulate a lifetime
-# vector and, independently, a termination time (=censoring time) vector. Then we observe
-# whichever comes first (ztimes) and related indicator (status)
+# To simulate the (randomly right) censored observations, we must first simulate a lifetime
+# and, independently, a termination time (=censoring) vector. Then, we observe
+# whichever comes first and the related indicator
 #
 # For the cured individuals, we simulate a time to cure identification 
 # Then, we follow the same process as above
@@ -13,9 +13,6 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
   
   # We start with determining the population ------------------------------
   set.seed(seed)
-  
-  # Total sample size
-  # N = 500
   
   # -------------------------------------------------------------------
   #                       1. Model Covariates generation
@@ -42,7 +39,7 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
   # -------------------------------------------------------------------
   
   # a. Generate the logistic regression
-  logit_incidence = 2 + cbind(z1, z2, q1, q2) %*% incidence_par
+  logit_incidence = cbind(rep(1,N), z1, z2, q1, q2) %*% incidence_par
   
   # b. Calculate the probabilities
   prob_incidence = plogis(logit_incidence)
@@ -65,7 +62,7 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
   # -------------------------------------------------------------------
   
   shape_par = 2
-  scale_par = 2 
+  scale_par = 2
   X = mydata[, c('x1', 'x2', 'q1', 'q2')] |> as.matrix()
   scale_par_indiv = scale_par * exp(- X %*% (latency_par / shape_par))
   
@@ -77,7 +74,7 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
   # -------------------------------------------------------------------
   
   shape_par_cure_id = 2
-  scale_par_cure_id = 1 
+  scale_par_cure_id = 2 
   X = mydata[, c('x1')] |> as.matrix()
   
   scale_par_indiv_cure_id = scale_par_cure_id * exp(- X %*% (cure_id_par / shape_par_cure_id))
@@ -101,11 +98,7 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
       observed_time = ifelse(censor_status == 1,
                              true_time,
                              cens_time),
-      z1, z2, q1, q2, x1, x2,     
-      censor_status0 = ifelse(censor_status == 1 & true_cure_status == 1, 1, 0),
-      observed_cure_status0 = ifelse(censor_status0 == 1, 1, NA),
-      observed_cure_status100 = true_cure_status,
-      censor_status100 = censor_status
+      z1, z2, q1, q2, x1, x2     
       
       )
   
@@ -113,31 +106,17 @@ myfunction = function(N, exp_rate, seed, prob_known_cured, incidence_par, latenc
   
   results = list()
   
-  results[['Data']] = df
-  
-  results[['Parameters']] = cbind(
-    
-    c('Sample_size', 'Censor', 'Beta', 'Gamma'),
-    c(N, paste0('Exp(', exp_rate, ')'),
-      paste0(incidence_par, collapse = '_'),
-      paste0(latency_par, collapse = '_'))
-    
-    
-  ) %>%
-    
-    as.data.frame()
-  
-  colnames(results$Parameters) = c('Parameter', 'Value')
-  
-  prob_known_cured = sum(mydata$censor_status == 1 & mydata$true_cure_status == 1) / sum(mydata$true_cure_status == 1)*100
+  results[['Data']] = mydata
   
   file_name = paste0('Mech2_', 'N_', N,
-                     '_Censor_Exp(', exp_rate, ')',
-                     '_Prob_known_', prob_known_cured |> round(),
+                     '_Shape_par(', shape_par,
+                     ')_Shape_par_Cure(', shape_par_cure_id,
+                     ')_Censor_Exp(', exp_rate, ')',
                      '_Beta_(', paste0(incidence_par, collapse = '_'),
-                     ')_Gamma_(', paste0(latency_par, collapse = '_'), ')')
+                     ')_Gamma_(', paste0(latency_par, collapse = '_'),
+                     ')_Cure_ID_(', paste0(cure_id_par, collapse = '_'), ')')
   
-  myfile_save = gsub('Rscripts/Simulations', paste0('Simulated_Datasets/', file_name,'//'), getwd())
+  myfile_save = gsub('Rscripts.*', paste0('Simulated_Datasets/', file_name,'//'), getwd())
   
   ifelse(!dir.exists(myfile_save), dir.create(myfile_save), FALSE)
   
@@ -156,14 +135,8 @@ for (j in myseed){
     myfunction(N = 500,
                exp_rate = 1/5,
                seed = j,
-               incidence_par = c(1.5, -2, 4, 0.5),
-               latency_par = c(0.9, 4, -2, 2),
-               cure_id_par = 0.5)
+               incidence_par = c(2, 4, 2, 4, 0.5),
+               latency_par = c(0.9, 1, 4, 2),
+               cure_id_par = 2)
     
 }
-  
-
-
-
-
-
